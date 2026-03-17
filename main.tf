02c0cf2c06f0f31250996046abbe14760f690871
Malli-WAS <haripriyadharshu@gmail.com>
2026-03-17 13:27:52 +0530

    commit all

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
-terraform {
+provider "aws" {
+  region     = "us-east-1"
   
+}
+
+resource "aws_iam_user" "demo_user" {
+  name = var.user_name
+}
+
+resource "aws_iam_user_policy_attachment" "attach_policy" {
+  user       = aws_iam_user.demo_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "user_name" {
+  description = "IAM User Name"
+  type        = string
+  default     = "terraform-demo-user"
+}
